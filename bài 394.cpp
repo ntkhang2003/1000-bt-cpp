d4e98a2f95c3022cf088fe6dce7af135e11485fb
ntkhang2003 <88282062+ntkhang2003@users.noreply.github.com>
2021-09-21 16:02:48 +0700

    Create bài 394.cpp

---
+++ bài 394.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+void nhap(int a[][100], int dong, int cot)
+{
+	for (int i = 0; i < dong; i++){
+		for (int j = 0; j < cot; j++){
+			cout << "Nhap a[" << i << "][" << j << "] = ";
+			cin >> a[i][j];
+		}
+	}
+}
+void xuat(int a[][100], int dong, int cot)
+{
+	for (int i = 0; i < dong; i++){
+		for (int j = 0; j < cot; j++){
+			cout << a[i][j] << " ";
+		}
+		cout << "\n";
+	}	
+}
+void HoanVi(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+void DichXuongXoayVong(int a[][100], int dong, int cot, int k)
+{
+	for (int m = 0; m < k; m++)
+	{
+		int temp[100];
+    	for(int i = 0; i < cot; i++)
+    	{
+        	temp[i] = a[dong - 1][i];
+    	}
+    	for(int i = dong - 1; i >= 1; i--)
+    	{
+        	for(int j = 0; j < cot; j++)
+        	{
+            	a[i][j] = a[i - 1][j];
+        	}
+   		}
+    	for(int i = 0; i < cot; i++)
+    	{
+        	a[0][i] = temp[i];
+		}
+	}	
+}
+int main()
+{
+	int a[100][100], dong, cot, k;
+	cout << "Nhap so dong: ";
+	cin >> dong;
+	cout << "Nhap so cot: ";
+	cin >> cot;
+	nhap(a, dong, cot);
+	xuat(a, dong, cot);
+	cout << "Nhap so lan: ";
+	cin >> k;
+	DichXuongXoayVong(a, dong, cot, k);
+	xuat(a, dong, cot);
+	return 0;
+}
