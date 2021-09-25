3de0d6dcb818c8f83db95773bb96acc56b18953a
ntkhang2003 <88282062+ntkhang2003@users.noreply.github.com>
2021-09-25 17:39:14 +0700

    Create bài 458.cpp

---
+++ bài 458.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+void nhap(int a[][100], int n)
+{
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++){
+			cout << "Nhap a[" << i << "][" << j << "] = ";
+			cin >> a[i][j];
+		}
+	}
+}
+void xuat(int a[][100], int n)
+{
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++){
+			cout << a[i][j] << " ";
+		}
+		cout << "\n";
+	}	
+}
+bool KiemTraMaTranCoDoiXungQuaDuongCheoPhu(int a[][100], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - 1 - i; j++)
+        {
+            if(a[i][j] != a[n - 1 - j][n - 1 - i])
+            {
+				return false;
+            }
+        }
+    }
+    return true;
+}
+int main()
+{
+	int a[100][100], n;
+	cout << "Nhap n: ";
+	cin >> n;
+	nhap(a, n);
+	xuat(a, n);
+	cout << KiemTraMaTranCoDoiXungQuaDuongCheoPhu(a ,n);
+	return 0;
+}
